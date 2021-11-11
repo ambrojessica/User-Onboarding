b830a2939825f808c77ab27c74b64b21cdeaff30
ambrojessica <ambrojessica0@gmail.com>
2021-11-10 18:49:05 -0600

    first commit

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,58 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+//import 
+import Form from './Form';
+// import axios from 'axios';
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  //checkbox
+  checked: false,
+  //button
+  submit: '',
+}
+
+const initialFormError = {
+  name: '',
+  email: '',
+  password: '',
+  //button
+  submit: '',
+}
+
+const initialApplication = []
+const initialDisabled = true
+
+
+function App() {
+
+  const [form, setForm] = useState(initialApplication)
+  // const [formValues, setFormValues] = useState(initialFormValues)
+  // const [formErrors, setFormErrors] = useState(initialFormError)
+  // const [disabled, setDisabled] = useState(initialDisabled)
+
+  const onChange = (name, value) => {
+    setForm({ ...form, [name]: value })
+  }
+
+  const onSubmit = () => {
+    
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={'https://www.logolynx.com/images/logolynx/1b/1be8160b5d2ef5b3254ecd8f378c8caf.png'} className="App-logo" alt="logo" />
+      
+      <Form
+      values={form}
+      change={onChange}
+      />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+const Form = (props) => {
+
+  const { change, submit, disabled } = props
+  const {name, email, password, checked} = props.values;
+
+  const onSubmit = evt => {
+    evt.preventDefault();
+    submit();
+  }
+
+  const onChange = evt => {
+    const {name, value, checked, type} = evt.target;
+    const completeValue = type === 'checkbox' ? checked : value;
+    change(name, completeValue);
+  }
+
+  return (
+    <form className='form' onSubmit={onSubmit}>
+      <div>
+      <h2>Create a NEW Account, it is SIMPLE and EASY!</h2>
+
+      <h3>Information Here 🡳 </h3>
+      <label>Full Name:
+      <input 
+        value={name}
+        onChange={onChange}
+        name='name'
+        type='text'
+      />
+      </label>
+
+      <label>Email:
+      <input 
+        value={email}
+        onChange={onChange}
+        name='email'
+        type='email'
+      />
+      </label>
+
+      <label>Password:
+      <input 
+        value={password}
+        name='first name'
+        type='password'
+        onChange={onChange}
+      />
+      </label>
+
+      <label>
+        <input 
+        type='checkbox'
+        name='tos'
+        checked={checked}
+        onChange={onChange}
+        />
+      </label>
+      <button disabled={disabled}>submit</button>
+      </div>
+
+    </form>
+  )
+}
+
+export default Form; 
